b50cae1a0b82dde451f8779b74a49f0a0b466a74
Olkash <savrikov.vladi.slav@mail.ru>
2026-01-14 21:00:31 +0700

    add first files

---
+++ CollegeSchedule/CollegeSchedule/Program.cs
@@ -0,0 +1,39 @@
+using CollegeSchedule.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Add services to the container.
+DotNetEnv.Env.Load();
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+ $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+ $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+ $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseNpgsql(connectionString));
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
